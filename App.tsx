4000136a456dfb8408a259d01b8a34e706b125d1
신승훈 <zlzlzlmo@daum.net>
2022-07-01 22:01:50 +0900

    bottom navigation setting 추가

--- App.tsx
+++ App.tsx
@@ -2,21 +2,27 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AllExpenses from "./src/screens/AllExpenses";
 import ManageExpenses from "./src/screens/ManageExpenses";
 import RecentExpenses from "./src/screens/RecentExpenses";
 
 const Stack = createStackNavigator();
+const Bottom = createBottomTabNavigator();
 
 export default function App() {
+  // * NavigationContainer은 앱 상태를 관리하고 앱 환경에 네비게이터를 연결한다
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="AllExpenses" component={AllExpenses} />
-        <Stack.Screen name="ManageExpenses" component={ManageExpenses} />
-        <Stack.Screen name="RecentExpenses" component={RecentExpenses} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="AllExpenses" component={AllExpenses} />
+          <Stack.Screen name="ManageExpenses" component={ManageExpenses} />
+          <Stack.Screen name="RecentExpenses" component={RecentExpenses} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
