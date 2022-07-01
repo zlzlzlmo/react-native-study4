8cf54b16e01edc2d46177e96c4883743e4e5fa4f
신승훈 <zlzlzlmo@daum.net>
2022-07-01 21:51:40 +0900

    react-navigation setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import AllExpenses from "./src/screens/AllExpenses";
+import ManageExpenses from "./src/screens/ManageExpenses";
+import RecentExpenses from "./src/screens/RecentExpenses";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="AllExpenses" component={AllExpenses} />
+        <Stack.Screen name="ManageExpenses" component={ManageExpenses} />
+        <Stack.Screen name="RecentExpenses" component={RecentExpenses} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ src/screens/RecentExpenses.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const RecentExpenses = () => {
+  return (
+    <View>
+      <Text>RecentExpenses</Text>
+    </View>
+  );
+};
+
+export default RecentExpenses;
+
+const styles = StyleSheet.create({});
