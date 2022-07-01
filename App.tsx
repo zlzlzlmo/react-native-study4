b8191a55d367bf2f7f6702a588542bf25879fb27
신승훈 <zlzlzlmo@daum.net>
2022-07-01 22:22:47 +0900

    bottom navigator header 및 tab bar 스타일링

--- App.tsx
+++ App.tsx
@@ -6,20 +6,64 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AllExpenses from "./src/screens/AllExpenses";
 import ManageExpenses from "./src/screens/ManageExpenses";
 import RecentExpenses from "./src/screens/RecentExpenses";
-
+import { GlobalStyles } from "./src/constants/styles";
+import { AntDesign } from "@expo/vector-icons";
 const Stack = createStackNavigator();
 const Bottom = createBottomTabNavigator();
 
+const ExpenseOverview = () => {
+  return (
+    <Bottom.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: GlobalStyles.colors.subColor3,
+        },
+        headerTintColor: GlobalStyles.colors.blackColor,
+        tabBarStyle: {
+          backgroundColor: GlobalStyles.colors.subColor3,
+        },
+        tabBarActiveTintColor: GlobalStyles.colors.blackColor,
+      }}
+    >
+      <Bottom.Screen
+        name="AllExpenses"
+        component={AllExpenses}
+        options={{
+          headerTitle: "모든 비용",
+          tabBarLabel: "전체",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="hourglass" size={size} color={color} />
+          ),
+        }}
+      />
+      <Bottom.Screen
+        name="RecentExpenses"
+        component={RecentExpenses}
+        options={{
+          headerTitle: "최근 비용",
+          tabBarLabel: "최근",
+        }}
+      />
+    </Bottom.Navigator>
+  );
+};
+
 export default function App() {
   // * NavigationContainer은 앱 상태를 관리하고 앱 환경에 네비게이터를 연결한다
+  // * headerShown 옵션으로 header를 숨길 수 있다. ( nested navigation을 사용하면 header가 두개 나오므로 상위 스크린 헤더를 안보이게 하기 위함 )
   return (
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="AllExpenses" component={AllExpenses} />
+          <Stack.Screen
+            name="ExpenseOverview"
+            component={ExpenseOverview}
+            options={{
+              headerShown: false,
+            }}
+          />
           <Stack.Screen name="ManageExpenses" component={ManageExpenses} />
-          <Stack.Screen name="RecentExpenses" component={RecentExpenses} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ src/constants/styles.ts
@@ -0,0 +1,10 @@
+export const GlobalStyles = {
+  colors: {
+    mainColor: "#b1e9a3",
+    subColor: "#7ac38f",
+    subColor2: "#88e0d0",
+    subColor3: "#c7eeff",
+    whiteColor: "#fff",
+    blackColor: "#000",
+  },
+};
