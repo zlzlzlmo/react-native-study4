0d280c727df22bbb920132292c8c749c5792a249
신승훈 <zlzlzlmo@daum.net>
2022-07-02 12:55:08 +0900

    뒤로가기 기능 추가

--- src/screens/ManageExpenses.tsx
+++ src/screens/ManageExpenses.tsx
@@ -21,11 +21,17 @@ const ManageExpenses = ({ route, navigation }: Props) => {
     });
   }, [navigation, isEditing]);
 
-  const deleteExpenseHandler = () => {};
+  const deleteExpenseHandler = () => {
+    navigation.goBack();
+  };
 
-  const cancelHandler = () => {};
+  const cancelHandler = () => {
+    navigation.goBack();
+  };
 
-  const confirmHandler = () => {};
+  const confirmHandler = () => {
+    navigation.goBack();
+  };
 
   return (
     <View style={styles.container}>
